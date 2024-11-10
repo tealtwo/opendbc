53c8fdec0a95e022c6ee88ffef3694aaf3c20798
DevTekVE <devtekve@gmail.com>
2024-11-10 21:43:19 +0100

    Refactor Hyundai flags for consistency
    Reorganized Hyundai flag values to avoid conflicts and improve readability. Reduced power values for each flag to simplify the flag structure and ensure unique identifiers for each feature.

--- opendbc/sunnypilot/car/hyundai/flags.py
+++ opendbc/sunnypilot/car/hyundai/flags.py
@@ -2,17 +2,14 @@ from enum import IntFlag
 
 
 class HyundaiFlagsSP(IntFlag):
-  """
-  Sunnypilot flags for hyundai. We reuse CarParams from OP. So we define the flags in reverse order to avoid conflicts.
-  """
-  SP_ENHANCED_SCC = 2**31
-  SP_CAN_LFA_BTN = 2**30
-  SP_NAV_MSG = 2 ** 29
+  SP_ENHANCED_SCC = 1
+  SP_CAN_LFA_BTN = 2
+  SP_NAV_MSG = 2 ** 2
 
-  SP_NON_SCC = 2 ** 28
-  SP_NON_SCC_FCA = 2 ** 27
-  SP_NON_SCC_RADAR_FCA = 2 ** 36
+  SP_NON_SCC = 2 ** 3
+  SP_NON_SCC_FCA = 2 ** 4
+  SP_NON_SCC_RADAR_FCA = 2 ** 5
 
-  SP_CAMERA_SCC_LEAD = 2 ** 25
-  SP_LKAS12 = 2 ** 24
-  SP_RADAR_TRACKS = 2 ** 23 # this one clashes with one in stock huindaiflags. We might need the new flags field afterall..
+  SP_CAMERA_SCC_LEAD = 2 ** 6
+  SP_LKAS12 = 2 ** 7
+  SP_RADAR_TRACKS = 2 ** 8
