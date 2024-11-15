445672072a14746b5c9e97e49bf4f3b032f43c34
DevTekVE <devtekve@gmail.com>
2024-11-15 08:05:42 +0100

    Refactor Hyundai flag definitions
    Updated HyundaiFlagsSP class to include documentation for special flags used within sunnypilot. The flag definitions were moved to bitwise shift notation for clarity and consistency, ensuring ease of extension up to 32 flags.

--- opendbc/sunnypilot/car/hyundai/flags.py
+++ opendbc/sunnypilot/car/hyundai/flags.py
@@ -2,14 +2,9 @@ from enum import IntFlag
 
 
 class HyundaiFlagsSP(IntFlag):
-  SP_ENHANCED_SCC = 1
-  SP_CAN_LFA_BTN = 2
-  SP_NAV_MSG = 2 ** 2
-
-  SP_NON_SCC = 2 ** 3
-  SP_NON_SCC_FCA = 2 ** 4
-  SP_NON_SCC_RADAR_FCA = 2 ** 5
-
-  SP_CAMERA_SCC_LEAD = 2 ** 6
-  SP_LKAS12 = 2 ** 7
-  SP_RADAR_TRACKS = 2 ** 8
+  """
+  Represents special flags for Hyundai specific features within sunnypilot.
+  These flags will go into the `sunnyParams.flags` field of the `CarParams` (car.capnp)
+  Max 32 flags can be defined in this class.
+  """
+  SP_ENHANCED_SCC = 1 << 0
