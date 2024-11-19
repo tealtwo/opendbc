6dde2c8435b0e09158ab455aa215a573f5212c11
Jason Wen <haibin.wen3@gmail.com>
2024-11-19 12:51:44 -0500

    MUST REMOVE test process replay

--- opendbc/car/hyundai/interface.py
+++ opendbc/car/hyundai/interface.py
@@ -90,9 +90,9 @@ class CarInterface(CarInterfaceBase):
       if ret.flags & HyundaiFlags.CAMERA_SCC:
         ret.safetyConfigs[0].safetyParam |= Panda.FLAG_HYUNDAI_CAMERA_SCC
 
-      if 0x391 in fingerprint[0]:
-        ret.sunnypilotFlags |= HyundaiFlagsSP.HAS_LFA_BUTTON.value
-        ret.safetyConfigs[-1].safetyParam |= Panda.FLAG_HYUNDAI_LFA_BUTTON
+      #if 0x391 in fingerprint[0]:
+        #ret.sunnypilotFlags |= HyundaiFlagsSP.HAS_LFA_BUTTON.value
+        #ret.safetyConfigs[-1].safetyParam |= Panda.FLAG_HYUNDAI_LFA_BUTTON
 
     # Common lateral control setup
 
