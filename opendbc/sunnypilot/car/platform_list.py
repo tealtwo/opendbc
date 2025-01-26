5167394bc8c125aed3f4df6edc7a236c2ccdcff6
Jason Wen <haibin.wen3@gmail.com>
2025-01-25 22:16:53 -0500

    generate extra list

--- opendbc/sunnypilot/car/platform_list.py
+++ opendbc/sunnypilot/car/platform_list.py
@@ -7,11 +7,18 @@ from opendbc.car.docs import get_all_footnotes, get_params_for_docs
 from opendbc.car.values import PLATFORMS
 
 CAR_LIST_JSON_OUT = os.path.join(BASEDIR, "../", "sunnypilot", "car", "car_list.json")
+CAR_LIST_WITH_EXTRAS_JSON_OUT = os.path.join(BASEDIR, "../", "sunnypilot", "car", "car_list_extra.json")
 
 
 def get_car_list() -> dict[str, str]:
   collected_footnote = get_all_footnotes()
   sorted_list: dict[str, str] = build_sorted_car_list(PLATFORMS, collected_footnote)
+  return sorted_list
+
+
+def get_car_list_with_extras() -> dict[str, dict[str, list[str] | str]]:
+  collected_footnote = get_all_footnotes()
+  sorted_list: dict[str, dict[str, list[str] | str]] = build_sorted_car_list_with_extras(PLATFORMS, collected_footnote)
   return sorted_list
 
 
@@ -37,9 +44,48 @@ def build_sorted_car_list(platforms, footnotes) -> dict[str, str]:
   return sorted_car_list
 
 
+def build_sorted_car_list_with_extras(platforms, footnotes) -> dict[str, dict[str, list[str] | str]]:
+  cars: dict[str, dict[str, list[str] | str]] = {}
+  for model, platform in platforms.items():
+    car_docs = platform.config.car_docs
+    CP = get_params_for_docs(model, platform)
+
+    if CP.dashcamOnly or not len(car_docs):
+      continue
+
+    # A platform can include multiple car models
+    for _car_docs in car_docs:
+      if not hasattr(_car_docs, "row"):
+        _car_docs.init_make(CP)
+        _car_docs.init(CP, footnotes)
+      cars[_car_docs.name] = model
+
+      _name = _car_docs.name
+      _model = model
+      _years = _car_docs.year_list
+      _package = _car_docs.package if _car_docs.package else []
+      _support = _car_docs.support_link if _car_docs.support_link else []
+
+      cars[_name] = {
+        "model": _model,
+        "year": _years if _years else [],
+        "package": _package,
+      }
+
+  # Sort cars by make and model + year
+  sorted_cars = natsorted(cars.keys(), key=lambda car: car.lower())
+  sorted_car_list = {car: cars[car] for car in sorted_cars}
+  return sorted_car_list
+
+
 if __name__ == "__main__":
-  platform_dict = get_car_list()
+  platform_list = get_car_list()
+  platform_list_with_extras = get_car_list_with_extras()
 
   with open(CAR_LIST_JSON_OUT, "w") as json_file:
-    json.dump(platform_dict, json_file, indent=2, ensure_ascii=False)
+    json.dump(platform_list, json_file, indent=2, ensure_ascii=False)
   print(f"Generated and written to {CAR_LIST_JSON_OUT}")
+
+  with open(CAR_LIST_WITH_EXTRAS_JSON_OUT, "w") as json_file:
+    json.dump(platform_list_with_extras, json_file, indent=2, ensure_ascii=False)
+  print(f"Generated and written to {CAR_LIST_WITH_EXTRAS_JSON_OUT}")
