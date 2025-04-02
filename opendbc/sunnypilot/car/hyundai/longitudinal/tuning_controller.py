79072d23e0ee79954a31c6226354fd3125adf74e
discountchubbs <alexgrant990@gmail.com>
2025-04-02 07:56:35 -0700

    Try SPSS lower jerk cal
    ** Note: May need to revert so keep that in mind **

--- opendbc/sunnypilot/car/hyundai/longitudinal/tuning_controller.py
+++ opendbc/sunnypilot/car/hyundai/longitudinal/tuning_controller.py
@@ -54,8 +54,8 @@ class LongitudinalTuningController:
 
     # Jerk is calculated using current accel - last accel divided by ΔT (delta time)
     current_accel = CS.out.aEgo
-    upper_band_jerk = (current_accel - self.state.accel_last_jerk) / 0.125
-    lower_band_jerk = (current_accel - self.state.accel_last_jerk) / 0.30
+    upper_band_jerk = (current_accel - self.state.accel_last_jerk) / 0.125    # This is fine for now
+    lower_band_jerk = (current_accel - self.state.accel_last_jerk) * 50.101   # Try SPSS calculated var
     self.state.accel_last_jerk = current_accel
 
     # Jerk is limited by the following conditions imposed by ISO 15622:2018
