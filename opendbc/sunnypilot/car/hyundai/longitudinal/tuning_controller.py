66b81c54ff6c99cdcaa150d4a14c4509babc3537
discountchubbs <alexgrant990@gmail.com>
2025-04-04 09:46:40 -0700

    Conditionally higher minimum jerk threshold.

--- opendbc/sunnypilot/car/hyundai/longitudinal/tuning_controller.py
+++ opendbc/sunnypilot/car/hyundai/longitudinal/tuning_controller.py
@@ -75,9 +75,10 @@ class LongitudinalTuningController:
       accel_jerk_max = self.car_config.jerk_limits[2]
 
     accel_jerk = accel_jerk_max if LongCtrlState == LongCtrlState.pid else 1.0
+    min_lower_jerk = self.car_config.jerk_limits[0] if (velocity > 6.5) else 0.9
 
     self.jerk_upper = min(max(self.car_config.jerk_limits[0], upper_band_jerk * 2.0), accel_jerk)
-    self.jerk_lower = min(max(0.9, -lower_band_jerk * 2.0), decel_jerk_max)
+    self.jerk_lower = min(max(min_lower_jerk, -lower_band_jerk * 2.0), decel_jerk_max)
 
   def calculate_accel(self, CC: structs.CarControl) -> float:
     """Calculate acceleration with cruise control status handling."""
