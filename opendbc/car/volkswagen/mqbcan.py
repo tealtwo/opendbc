3a02884ba2b0aad36de49b0f6631dea398555c67
Teal <teal@teallvbs.xyz>
2025-06-18 15:27:37 -0500

    VW PQ: bugfix8

--- opendbc/car/volkswagen/mqbcan.py
+++ opendbc/car/volkswagen/mqbcan.py
@@ -80,7 +80,7 @@ def acc_control_value(main_switch_on, acc_faulted, long_active):
   return acc_control
 
 
-def acc_hud_status_value(main_switch_on, acc_faulted, long_active):
+def acc_hud_status_value(main_switch_on, acc_faulted, long_active, cruiseOverride):
   # TODO: happens to resemble the ACC control value for now, but extend this for init/gas override later
   return acc_control_value(main_switch_on, acc_faulted, long_active)
 
