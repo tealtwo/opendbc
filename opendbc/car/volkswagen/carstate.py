c74b012b3164fefc36607ae09346287a545e6c6e
Teal <teal@teallvbs.xyz>
2025-07-29 16:20:41 -0500

    bugfix

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -247,7 +247,7 @@ class CarState(CarStateBase):
     self.bremse11_stock = pt_cp.vl["Bremse_11"]
     self.LH2_steeringState = pt_cp.vl["Lenkhilfe_2"]["LH2_aktLenkeingriff"]
     self.LH2_Abbr = pt_cp.vl["Lenkhilfe_2"]["LH2_PLA_Abbr"]
-    self.MOB_Standby = br_cp.vl["Motor_Bremse"]["MOB_Standby]"]
+    self.MOB_Standby = br_cp.vl["Motor_Bremse"]["MOB_Standby"]
 
     # Update button states for turn signals and ACC controls, capture all ACC button state/config for passthrough
     ret.leftBlinker, ret.rightBlinker = self.update_blinker_from_stalk(300, pt_cp.vl["Gate_Komf_1"]["GK1_Blinker_li"],
