c779993e09788a109b8e84256901f0cdb22b8aa6
Teal <teal@teallvbs.xyz>
2025-07-30 15:58:31 -0500

    VW PQ: rework cruiseOverride logic loop in a desperate attempt to make it work like oeLong for opLong

--- opendbc/car/volkswagen/pqcan.py
+++ opendbc/car/volkswagen/pqcan.py
@@ -68,12 +68,14 @@ def acc_control_value(main_switch_on, long_active, cruiseOverride):
   return acc_control
 
 
-def acc_hud_status_value(main_switch_on, acc_faulted, acc_control, gasPressed):
+def acc_hud_status_value(main_switch_on, acc_faulted, acc_control, gasPressed, longActive):
   # gasPressed = cruiseOverride
   if acc_faulted:
     hud_status = 6
   elif acc_control == 1:
-    hud_status = 4 if gasPressed else 3
+    hud_status = 3
+  elif gasPressed and longActive:
+    hud_status = 4
   elif main_switch_on:
     hud_status = 2
   else:
