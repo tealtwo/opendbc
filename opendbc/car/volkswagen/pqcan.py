aa1db700de0c2763f389a237dc151fc380035746
T3vl <80123588+tealtwo@users.noreply.github.com>
2025-08-16 14:34:03 -0500

    VW PQ: change or function to and function to prevent false cruiseState override sending ACA StaACC status 4 incorrectly

--- opendbc/car/volkswagen/pqcan.py
+++ opendbc/car/volkswagen/pqcan.py
@@ -69,7 +69,7 @@ def acc_control_value(main_switch_on, long_active, cruiseOverride):
 
 
 def acc_hud_status_value(main_switch_on, acc_faulted, gasPressed, longActive, longOverride):
-  if gasPressed or longOverride and longActive:
+  if gasPressed and longOverride and longActive:
     hud_status = 4
   elif not longOverride and longActive:
     hud_status = 3
