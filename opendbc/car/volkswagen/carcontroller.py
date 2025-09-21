be2663a509b146a2f4e976d9237248558da7e136
teal <teal@teallvbs.xyz>
2025-09-21 13:25:40 -0500

    VW PQ: fix logic bug, we always have a lead distance bar, this is NOT lead distance.

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -303,14 +303,14 @@ class CarController(CarControllerBase):
       lead_distance = 0
       # Handle leadDistanceBars for VW PQ Kombi MFD
       if hud_control.leadVisible and self.frame * DT_CTRL > 1.0:  # Don't display lead until we know the scaling factor
-        self.leadDistanceBars = min(3, hud_control.leadDistanceBars) if hud_control.leadVisible else 0
+        self.leadDistanceBars = min(3, hud_control.leadDistanceBars)
         if self.leadDistanceBars == 1:
           distanceBars = 2
         elif self.leadDistanceBars == 2:
           distanceBars = 4
         elif self.leadDistanceBars == 3:
           distanceBars = 8
-        elif not self.leadDistanceBars or self.leadDistanceBars == 0:
+        elif self.leadDistanceBars == 0:
           distanceBars = 0
       else:
         distanceBars = 0
